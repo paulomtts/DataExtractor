ef33f0f495060cc59d018589170211295e55eab6
paulomtts <paulomtts@outlook.com>
2022-09-04 01:25:43 -0300

    complete config rework & backend adaptation for gui

--- app_classes.py
+++ app_classes.py
@@ -3,19 +3,24 @@ from tika import parser as tkpr
 
 import re
 
-AVOID_KEYS = ['layout', 'pk_area', 'pk_name']
-
 class Extract():
-    def __init__(self, query_patterns: list, **kwargs) -> None:
+    def __init__(self, pk_data: dict, query_ptts: list, **kwargs) -> None:
         for key, val in kwargs.items():
             setattr(self, key, val)
 
-        self.query_ptts    :dict    = query_patterns
+        self.pk_area       :str     = pk_data['area']
+        self.pk_key        :str     = pk_data['key']
+        self.pk            :str     = None
+
+        self.query_ptts    :dict    = query_ptts
         self.properties    :dict    = {key:{} for key in kwargs.keys()} # {area_name: {field_name: value}
 
     # METHODS #####################################################
     def set_properties(self, search_data: dict):
         """Set properties for all items in a dictionary given in a key:word_list pattern."""
+        
+        search_data[self.pk_area].insert(0, self.pk_key)
+        
         for area, keywords in search_data.items():
             text = getattr(self, area)
 
@@ -27,6 +32,9 @@ class Extract():
                 key = srch.groupdict().get('name', None)
                 val = srch.groupdict()['value'].strip()
                 self.properties[area].update({key: val})
+        
+        del search_data[self.pk_area][0]
+        self.pk = self.properties[self.pk_area][self.pk_key]
     
     def acquire_words(self, area: str, pattern: str):
         """Get a list of all field words in an area."""
@@ -42,10 +50,10 @@ class Entity():
         self.dataframe      :DataFrame       = None
 
     # METHODS #####################################################
-    def gather_extracts(self, extract_lst: list, pk_area: str, pk_name: str):
+    def gather_extracts(self, extract_lst: list):
         """Group Extract objects onto corresponding Entity."""
         for ext in extract_lst:
-            if ext.properties[pk_area][pk_name] == self.pk:
+            if ext.pk == self.pk:
                 self.extracts.append(ext)
 
     def build_dataframe(self, search_data: dict):
@@ -59,47 +67,47 @@ class Entity():
 
 class Document():
     def __init__(self, path: str, name: str) -> None:
-        natural_text   :str    = tkpr.from_file(f'{path}/{name}')['content']
+        self.natural_text   :str    = tkpr.from_file(f'{path}/{name}')['content']
         line_lst       :list   = []
 
-        for ln in natural_text.split('\n'):
+        for ln in self.natural_text.split('\n'):
             if ln == '': continue
             line_lst.append(ln.strip())
 
-        self.name      :str    = name
+        self.name      :str    = name.replace('.pdf', '')
         self.text      :str    = '\n'.join(line_lst)
         self.entities  :list   = []
         self.extracts  :list   = []
         self.word_dct  :list   = {}
 
     # METHODS #####################################################
-    def set_extracts(self, pattern_dct: dict):
+    def set_extracts(self, dct: dict):
         """Set Extracts containing useful text from this Document's text."""
-        layout_pattern = re.compile(pattern_dct['layout'], re.M)
-        query_patterns = {key: val[0] for key, val in pattern_dct.items() if key != 'layout'}
+        layout_pattern = re.compile(dct['MAIN']['layout'], re.M)
+        query_patterns = {key: val for key, val in dct['MAIN'].items() if key != 'layout'}
+        pk_data = dct['PK']
 
         for res in re.finditer(layout_pattern, self.text):
-            self.extracts.append(Extract(query_patterns, **res.groupdict()))
+            self.extracts.append(Extract(pk_data, query_patterns, **res.groupdict()))
     
-    def set_entities(self, pk_area: str, pk_name: str):
+    def set_entities(self):
         """Setup a new Entity for every key in a list of primary keys."""
-        pk_list = set([ext.properties[pk_area][pk_name] for ext in self.extracts])
+        pk_list = set([ext.pk for ext in self.extracts])
         self.entities = [Entity(pk) for pk in pk_list]
 
-    def set_words(self, area_lst: list, avoid: list, pattern_dct: dict):
+    def set_words(self, dct: dict, avoid: list):
         """Get list of words within a certain area in all extracts."""
-        ext: Extract
-
         filter_word = lambda word: word if not any(av in word for av in avoid) and len(word) > 2 else None
-        self.word_dct = {area: [] for area in area_lst if area != 'layout'}
 
-        for area in area_lst:
-            if area == 'layout': continue
-            if pattern_dct.get(area, None) is None: continue
+        self.word_dct = {area: [] for area in dct.keys()}
 
+        ext: Extract
+        for area in dct.keys():
             for ext in self.extracts:
-                words =  ext.acquire_words(area, pattern_dct[area][1])
+                words =  ext.acquire_words(area, dct[area])
                 words = list(filter(filter_word, words))
                 self.word_dct[area] += words
 
             self.word_dct[area] = list(set(self.word_dct[area]))
+
+
--- main.py
+++ main.py
@@ -1,154 +1,122 @@
-from aux_funcs import format_date, format_number
+from os import path as os_path, listdir as os_listdir
 from app_classes import Document, Entity, Extract
-from configparser import ConfigParser
-from xlwings import App, Book
+from aux_funcs import format_date, format_number
+from xlwings import App, Book, Sheet
 from datetime import datetime
-from pandas import read_excel
-from time import sleep
-from tika import initVM
-from os import path
+from pandas import DataFrame
 
-########################################################################################################################################################################################################################
-ROOT_PATH = path.dirname(__file__).replace('/app', '').replace('\\', '/')
+import json
 
-CONFIG_INI = ConfigParser()
-CONFIG_INI.read_file(open(f'{ROOT_PATH}/app/config.ini', "r", encoding="utf8"))
+PATH = os_path.dirname(__file__)
+TIMESTAMP = lambda: str(datetime.now().strftime('%y-%m-%dT%H-%M'))
+with open(f'{PATH}/app/config.json', encoding='utf-8') as json_file:
+    CONFIG = json.load(json_file)
 
-CONFIG_XL = {}
-for row in read_excel(f'{ROOT_PATH}/app/extractor.xlsm').iterrows():
-    file_name = str(row[1][0]).strip().rstrip('.pdf')
-    date_key = str(row[1][1]).strip()
-    id_key = str(row[1][2])
-    CONFIG_XL[file_name] = {
-        'id_numbers'    : [el.strip() for el in str(row[1][3]).split(',')],
-        'header_kwrds'  : [el.strip() for el in str(row[1][4]).split(',')],
-        'body_kwrds'    : [el.strip() for el in str(row[1][5]).split(',')],
-        'footer_kwrds'  : [el.strip() for el in str(row[1][6]).split(',') if el != 'nan'],
-    }
+# MAIN METHODS #############################################################################################
+def analyze(folder_path: str):
+    """Pre-process the text by setting up extracts and acquiring lists of words."""
+    files = [(folder_path, file_name) for file_name in os_listdir(folder_path) if '.pdf' in file_name]
+    avoid_words = from_gui('avoid_words')
 
-# Initialize tika
-initVM()
-
-########################################################################################################################################################################################################################
-def setup_objects():
-    """Setup Document and Group objects objects."""
-
-    doc_lst  = [Document(f'{ROOT_PATH}/documents', f'{file_name}') for file_name in CONFIG_XL.keys()]
+    doc_lst  = [Document(path, name) for (path, name) in files]
     for doc in doc_lst:
-        doc.set_entities([id_number for id_number in CONFIG_XL[doc.name]['id_numbers']])
+        for dct in CONFIG.values():
+            doc.set_extracts(dct)
+            doc.set_words(dct['WORDS'], avoid_words)
+    return doc_lst
 
-now = lambda: datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-"Return the date and time in standard format."
-
-def main():
-    """Extract data from all files and parse them into separate excel files."""
+def extract(doc_lst: list):
+    """Extract data from the the document and send it to excel."""
     doc: Document; ent: Entity; ext: Extract
 
-    # Set variables
-    drop_cols = ['MONTH', 'YEAR', date_key, id_key]
-
-    text_ptt = CONFIG_INI['EXPRESSIONS']['text']
-    header_ptt = CONFIG_INI['EXPRESSIONS']['header'].format("%")
-    body_ptt = CONFIG_INI['EXPRESSIONS']['body'].format("%")
-    footer_ptt = CONFIG_INI['EXPRESSIONS']['footer'].format("%")
+    search_data = from_gui('search_data')
     
-    # Setup document and group objects
-    print('Preparing Document and Block objects...')
-    setup_objects()
+    for doc in doc_lst:
 
-    # Extract data from documents
-    for doc in Document.lst:
-        global TIMESTAMP
-        
-        # Set additional variables
-        TIMESTAMP = str(now()).replace(' ', '_').replace(':', '-')
-        header_kwrds = CONFIG_XL[doc.name]['header_kwrds']
-        body_kwrds = CONFIG_XL[doc.name]['body_kwrds']
-        footer_kwrds = CONFIG_XL[doc.name]['footer_kwrds']
-        
-        print('Done!')
-        print('Extracting data...')
-        doc.set_extracts(text_ptt)
+        # Write natural .txt
+        with open(f'{PATH}/app/extracts/naturals/{TIMESTAMP()}__{doc.name}__nat.txt', 'w', encoding='utf-8') as file:
+            file.write(doc.natural_text)
 
-        # Log line list
-        with open(f'{ROOT_PATH}/app/extracts/naturals/{TIMESTAMP}_{file_name}_nat.txt', 'w', encoding='UTF-8') as fl:
-            for ln in doc.line_lst:
-                fl.write(ln + '\n')
+        # Write extract .txt
+        for ext in doc.extracts:
+            ext.set_properties(search_data)
 
-        # Log extracted data
-        with open(f'{ROOT_PATH}/app/extracts/{TIMESTAMP}_{doc.name}_ext.txt', 'w', encoding='UTF-8') as fl:
-            for obj in Extract.lst:
-                obj: Extract
-                fl.write(obj.header + '\n')
-                fl.write(obj.body+ '\n')
-                fl.write(obj.footer+ '\n')
-                fl.write('------\n')
+            with open(f'{PATH}/app/extracts/{TIMESTAMP()}__{doc.name}__ext.txt', 'a', encoding='utf-8') as file:
+                for prop in ext.properties.keys():
+                    file.write(getattr(ext, prop) + '\n')
+                file.write('----\n')
 
-        # Extract properties from data
-        print('Acquiring properties for each segment...')
-        for ext in Extract.lst:
-            ext.set_properties(ext.header, header_ptt, header_kwrds)
-            ext.set_properties(ext.body, body_ptt, body_kwrds)
-            ext.set_properties(ext.footer, footer_ptt, footer_kwrds)
-
-        # Assign blocks to groups and build dataframes from properties
-        print('Assigning Blocks to Groups...')
+        doc.set_entities()
         for ent in doc.entities:
-            ent.acquire_extracts('header', id_key)
+            ent.gather_extracts(doc.extracts)
+            ent.build_dataframe(search_data)
+            dataframe = treat_dataframe(ent.dataframe, 'Data de Crédito')
+            to_excel(dataframe, doc.name, ent.pk.replace('.', ''))
 
-            header = ent.build_dataframe(header_kwrds, 'header')
-            body   = ent.build_dataframe(body_kwrds, 'body')
-            footer = ent.build_dataframe(footer_kwrds, 'footer')
+# OUTPUT METHODS ###########################################################################################
+def treat_dataframe(dataframe: DataFrame, date_col: str):
+    avoid_cols = ['YEAR', 'MONTH'] + [date_col]
 
-            # Treat data and join dataframes
-            df = header.join([body, footer])
-            df = df.fillna('0')
+    dataframe = dataframe.fillna('0')
+    dataframe[date_col] = dataframe[date_col].apply(format_date)
+    dataframe['MONTH'] = dataframe[date_col].apply(lambda val: val.month)
+    dataframe['YEAR'] = dataframe[date_col].apply(lambda val: val.year)
+    dataframe = dataframe.reindex(columns=['YEAR', 'MONTH'] + [col for col in dataframe.columns if col not in avoid_cols])
 
-            df[date_key] = df[date_key].apply(format_date)
-            df['MONTH'] = df[date_key].apply(lambda val: val.month)
-            df['YEAR'] = df[date_key].apply(lambda val: val.year)
-            df = df.reindex(columns=['YEAR', 'MONTH'] + [col for col in df.columns if col not in drop_cols])
+    for col in [col for col in dataframe.columns if col not in avoid_cols]:
+        dataframe[col] = dataframe[col].apply(format_number)
 
-            for col in [col for col in df.columns if col not in drop_cols]:
-                df[col] = df[col].apply(format_number)
+    dataframe = dataframe.drop_duplicates()
+    dataframe = dataframe.sort_values(['YEAR', 'MONTH'])
+    dataframe = dataframe.reset_index(drop=True)
+    return dataframe
 
-            df = df.sort_values(['YEAR', 'MONTH'])
-            df = df.drop_duplicates()
-            df = df.reset_index(drop=True)
-
-        # Write to excel
-        print(f'{doc.name} - Writing to excel file...')
-        App(visible=False)
-        wb = Book()
-        ws = wb.sheets[0]
-
-
-        pos = 2
-        ws.range(f'A1').value = list(df.columns)
+def to_excel(dataframe: DataFrame, doc_name: str, ent_id: str):
+    """Write data to excel."""
+    wb: Book; ws: Sheet
+    
+    # Setup excel
+    App(visible=False)
+    wb = Book()
+    ws = wb.sheets[0]
+    try:
+        # Format table headers
+        ws.range(f'A1').value = list(dataframe.columns)
         ws.range(f'A1:AAA1').font.bold = True
         ws.range(f'A1:AAA1').font.size = 14
-        for year in df['YEAR'].unique():
-            slice = df[df['YEAR']==year]
+
+        # Write data
+        pos = 2
+        for year in dataframe['YEAR'].unique():
+            slice = dataframe[dataframe['YEAR']==year]
             ws.range(f'A{pos}').options(index=False, header=False).value = slice
             pos += len(slice.index)+1
 
+        # Fit columns and rows
         for ws in wb.sheets:
                 ws.autofit(axis="columns")
+                ws.autofit(axis="rows")
 
-        ws.range(f'$A1:$Z999').api.HorizontalAlignment = -4108
-
-        wb.save(f'{ROOT_PATH}/spreadsheets/{TIMESTAMP}__{doc.name}.xlsx')
-        wb.close()
-        print('\tDone!')
-        
-        sleep(1)
-
-if __name__ == "__main__":
-    try:
-        main()
-    except Exception as error:
-        print(f'{TIMESTAMP} - {error}')
-        with open(f'{ROOT_PATH}/app/log.txt', 'a') as file:
-            file.write(str(now()) + f' - {error}\n')
+        # Save
+        wb.save(f'{PATH}/spreadsheets/{TIMESTAMP()}__{doc_name}__{ent_id}.xlsx')
+    except:
+        pass
     finally:
-        print('Stopping graciously...')
+        wb.close()
+
+
+
+#####################################################
+# SINCE THIS WILL COME FROM GUI, EVERY ITERATION WILL GET IT'S PROPER SET
+def from_gui(label: str):
+    avoid_words = ['DESCONTOS', 'TOTAL', 'LIQUIDO', '/', 'NUM.', 'VALOR']
+    search_data = {'header': ['Data de Crédito'], 
+                'body': ['Ordenado', 'Reemb.Férias no Mês', 'Adiant.Vale Transporte', 'ORDENADO']}
+    if label == 'avoid_words':
+        return avoid_words
+    elif label == 'search_data':
+        return search_data
+#####################################################
+
+docs = analyze(f'{PATH}/documents/')       # Used by GUI access word lists in each document
+extract(docs)                              # This happens when you click EXTRACT
